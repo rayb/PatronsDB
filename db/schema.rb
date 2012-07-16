fa08ff1d6d00573c306556c05bafc0f7226f7d8b
Ray Bunnage <rayb@seattlespineinstitute.com>
2012-07-16 15:27:21 -0700

    added brown paper tickets csv file for daily ticket purchases forgot to check in updated shema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120715210108) do
+ActiveRecord::Schema.define(:version => 20120716011628) do
+
+  create_table "plays", :force => true do |t|
+    t.string   "name"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "performances"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "test_patrons", :force => true do |t|
     t.string   "last"
