b3ef52927acdbc291c35ca555617e65c6789654b
Ray Bunnage <rayb@seattlespineinstitute.com>
2012-07-23 20:59:31 -0700

    seeded patrons db and added one filter on the patron model as an example for Sonya

--- app/models/patron.rb
+++ app/models/patron.rb
@@ -11,6 +11,10 @@ class Patron < ActiveRecord::Base
   belongs_to :user
   has_many :purchases
 
+  def self.filtered_by(filter)
+    puts "filter from model: #{filter}"
+    where("last like '%#{filter}%'")
+  end
 
   attr_accessible :address_1, :address_2, :address_key, :city, :email, :entry_user_id, :first, :is_cast, :is_industry, :is_patron, :is_press, :last, :notes, :organization, :phone, :retiring_date, :source_id, :start_date, :state, :ticket_notes, :zip
 end
